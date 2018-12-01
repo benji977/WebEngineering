e5a23a987ca39bb0a86e754bbfe32740bbfc9d81
Benjamin <benjamin.probst@hispeed.ch>
2018-12-01 18:42:18 +0100

    test

--- pages/productive/TestBeni.php
+++ pages/productive/TestBeni.php
@@ -7,6 +7,6 @@
  */
 
 echo "Daten gespeichert";
-$db = mysqli_connect ("localhost", "root", "WebEng2018", "aaa_inventar");
+$db = mysqli_connect ("probst.synology.me", "root", "WebEng2018", "aaa_inventar");
 $insert = "INSERT INTO `tbl_login` (`ID`, `ID_users`, `pw`, `role`, `imgURL`) VALUES (NULL, '234', 'test', 'User', 'dist/img/login/user-dummy.png')";
 mysqli_query ($db, $insert);
