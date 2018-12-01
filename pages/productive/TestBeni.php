4254c9434d4304fb7e444c7e2d0285c5dac1d0cc
Benjamin <benjamin.probst@hispeed.ch>
2018-12-01 16:20:25 +0100

    test

---
+++ pages/productive/TestBeni.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benjamin
+ * Date: 01.12.2018
+ * Time: 16:17
+ */
+
+echo "Daten gespeichert";
+$db = mysqli_connect ("localhost", "root", "WebEng2018");
+mysqli_select_db($link, "aaa_inventar");
+$insert = "INSERT INTO `tbl_login` (`ID`, `ID_users`, `pw`, `role`, `imgURL`) VALUES (NULL, \'234\', \'test\', \'User\', \'dist/img/login/user-dummy.png\')";
+mysqli_query ($db, $insert);
+$mysqli_close($db);
