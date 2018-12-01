c5e8d1112ac2e8e38e5fe1e0ac24d84c973345a1
camillo.schweizer <camillo.schweizer@gmail.com>
2018-12-01 14:08:12 +0100

    php suck balls part 2

--- pages/productive/userform.php
+++ pages/productive/userform.php
@@ -1,15 +1,16 @@
 <?PHP
 echo "<H1>Ihre Daten wurden in die Tabelle \"adressen\" eintragen</H1><br/>\n";
-$feld1=$_POST['NR'];
-$feld2=$_POST['name'];
-$feld3=$_POST['strasse'];
-$feld4=$_POST['ort'];
+$feld1=$_POST['email'];
+$feld2=$_POST['exempleInputPassword1'];
+$feld3=$_POST['Vorname'];
+$feld4=$_POST['Nachname'];
+$feld5=$_POST['Arbeitsort'];
 $benutzer = "root";
-$passwort = "test";
-$dbname = "test_datenbank";
+$passwort = "camillo";
+$dbname = "webengineering";
 $link=mysqli_connect("localhost", $benutzer, $passwort);
 mysqli_select_db($link, $dbname);
-$insert = "INSERT INTO adressen (NR, name, strasse, ort) values ('$feld1', '$feld2', '$feld3', '$feld4')";
+$insert = "INSERT INTO benutzer (mail, passwort, vorname, name, arbeitsort) values ('$feld1', '$feld2', '$feld3', '$feld4', '$feld5')";
 $db = mysqli_query($link, "$insert") or die(mysqli_error());
 mysqli_close($link);
 ?>
