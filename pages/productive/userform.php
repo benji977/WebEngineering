b8318dd528309a552658d85936eb7d4cf195a634
camillo.schweizer <camillo.schweizer@gmail.com>
2018-12-02 12:17:23 +0100

    db prod

--- pages/productive/userform.php
+++ pages/productive/userform.php
@@ -1,16 +1,18 @@
 <?PHP
 echo "<H1>Ihre Daten wurden in die Tabelle \"adressen\" eintragen</H1><br/>\n";
+
 $feld1=$_POST['email'];
-$feld2=$_POST['exempleInputPassword1'];
-$feld3=$_POST['Vorname'];
-$feld4=$_POST['Nachname'];
-$feld5=$_POST['Arbeitsort'];
+$feld2=$_POST['password'];
+$feld3=$_POST['surname'];
+$feld4=$_POST['lastname'];
+$feld5=$_POST['place'];
 $benutzer = "root";
-$passwort = "camillo";
+$passwort = "WebEng2018";
 $dbname = "webengineering";
-$link=mysqli_connect("localhost", $benutzer, $passwort);
+$link=mysqli_connect("probst.synology.me", $benutzer, $passwort);
 mysqli_select_db($link, $dbname);
-$insert = "INSERT INTO benutzer (mail, passwort, vorname, name, arbeitsort) values ('$feld1', '$feld2', '$feld3', '$feld4', '$feld5')";
-$db = mysqli_query($link, "$insert") or die(mysqli_error());
+$insert = "INSERT INTO user (id, mail, password, surname, lastname, place) values ('1', '$feld1', '$feld2', '$feld3', '$feld4', '$feld5')";
+$db = mysqli_query($link, "$insert") or die(mysqli_error($link));
 mysqli_close($link);
+
 ?>
