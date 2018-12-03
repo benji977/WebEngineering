2101358729ebb276fb52ced7e9fa86663875e4e9
camillo.schweizer <camillo.schweizer@gmail.com>
2018-12-03 22:12:29 +0100

    small changes

--- pages/productive/contactform.php
+++ pages/productive/contactform.php
@@ -15,7 +15,7 @@ if(empty($_SESSION['usermail'])){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Benutzer erfassen</title>
+    <title>Kontakt erfassen</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
