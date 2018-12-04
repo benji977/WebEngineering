8c2b9ff904a7572acb7852f51cf8268b7c3f28e5
camillo.schweizer <camillo.schweizer@gmail.com>
2018-12-04 21:25:44 +0100

    beautyful login

--- pages/productive/login.php
+++ pages/productive/login.php
@@ -101,7 +101,7 @@ session_start();
 
         ?>
 
-        <form action="./login.php\" method="post">
+        <form action="login.php" method="post">
             <div class="form-group has-feedback">
                 <input type="email" class="form-control" name="mail" id="mail" required placeholder="Email">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
