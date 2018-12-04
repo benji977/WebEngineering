1ef89da44e32dba61947c92aa8a5a03a1e280a59
camillo.schweizer <camillo.schweizer@gmail.com>
2018-12-04 20:23:58 +0100

    path changes

--- pages/productive/logintodb.php
+++ pages/productive/logintodb.php
@@ -1,59 +1,68 @@
 <?PHP
 session_start();
 
-if(empty($_POST['mail'])){
-    echo "<meta http-equiv=\"refresh\" content=\"0; URL=login.php\">";
+if (!isset($_SESSION['usermail']) OR !isset($_SESSION['usersurname']) OR !isset($_SESSION['userlastname'])){
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL =login.php\">";
 }ELSE {
 
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
-    $benutzer = "root";
-    $passwort = "WebEng2018";
-    $dbname = "webengineering";
+    $usermail = $_SESSION['usermail'];
+    $usersurname = $_SESSION['usersurname'];
+    $userlastname = $_SESSION['userlastname'];
 
-
-    $link = mysqli_connect("probst.synology.me", $benutzer, $passwort);
-    mysqli_select_db($link, $dbname);
-    $select = "SELECT COUNT(*) AS sum FROM user WHERE mail = '$mail'";
-    $db = mysqli_query($link, "$select") or die(mysqli_error($link));
-
-
-    $row = mysqli_fetch_assoc($db);
-    $sum = $row['sum'];
-
-
-    if ($sum == 0) {
-
-        ?>
-        <script>alert("Nutzer nicht registriert");</script>
-        <?php
-
+    if (empty($_POST['mail'])) {
         echo "<meta http-equiv=\"refresh\" content=\"0; URL=login.php\">";
-    }
+    } ELSE {
+
+        $mail = $_POST['mail'];
+        $password = $_POST['password'];
+        $benutzer = "root";
+        $passwort = "WebEng2018";
+        $dbname = "webengineering";
 
 
-    if ($sum > 0) {
+        $link = mysqli_connect("probst.synology.me", $benutzer, $passwort);
+        mysqli_select_db($link, $dbname);
+        $select = "SELECT COUNT(*) AS sum FROM user WHERE mail = '$mail'";
+        $db = mysqli_query($link, "$select") or die(mysqli_error($link));
 
-        $passworddb = $link->query("SELECT password FROM user WHERE mail = '$mail'")->fetch_object()->password;
 
-        if (password_verify($password, $passworddb)) {
-            $_SESSION['usermail'] = $mail;
-            $_SESSION['usersurname'] = $link->query("SELECT surname FROM user WHERE mail = '$mail'")->fetch_object()->surname;
-            $_SESSION['userlastname'] = $link->query("SELECT lastname FROM user WHERE mail = '$mail'")->fetch_object()->lastname;
-            echo "<meta http-equiv=\"refresh\" content=\"0; URL=../../index.php\">";
-        } else {
+        $row = mysqli_fetch_assoc($db);
+        $sum = $row['sum'];
+
+
+        if ($sum == 0) {
+
             ?>
-            <script>alert("Passwort falsch");</script>
+            <script>alert("Nutzer nicht registriert");</script>
             <?php
 
             echo "<meta http-equiv=\"refresh\" content=\"0; URL=login.php\">";
         }
 
+
+        if ($sum > 0) {
+
+            $passworddb = $link->query("SELECT password FROM user WHERE mail = '$mail'")->fetch_object()->password;
+
+            if (password_verify($password, $passworddb)) {
+                $_SESSION['usermail'] = $mail;
+                $_SESSION['usersurname'] = $link->query("SELECT surname FROM user WHERE mail = '$mail'")->fetch_object()->surname;
+                $_SESSION['userlastname'] = $link->query("SELECT lastname FROM user WHERE mail = '$mail'")->fetch_object()->lastname;
+                echo "<meta http-equiv=\"refresh\" content=\"0; URL=../../index.php\">";
+            } else {
+                ?>
+                <script>alert("Passwort falsch");</script>
+                <?php
+
+                echo "<meta http-equiv=\"refresh\" content=\"0; URL=login.php\">";
+            }
+
+        }
+
+
+        mysqli_close($link);
+
     }
-
-
-    mysqli_close($link);
-
 }
 
 ?>
